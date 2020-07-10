c24232002c7d8673b98065123008149386d3b82f
Jay Sampat <jaydsampat@gmail.com>
2020-07-10 16:20:12 +0200

    Combat Testing
    -Twin stick shooting
    -Recoil force

---
+++ RogueLikeProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("XMove") * moveSpeed;
+        float moveY = Input.GetAxis("YMove") * moveSpeed;
+        Debug.Log(moveX + " " + moveY);
+        rb.velocity = new Vector2(moveX,moveY);
+    }
+}
---
+++ RogueLikeProject/Assets/Scripts/ShootingBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBehavior : MonoBehaviour
+{
+    [SerializeField] private bool hasRecoil;
+    [SerializeField] private float recoilForce;
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float rateOfFire;
+    float timeToFire;
+    bool isShooting;
+
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Rigidbody2D recoilObject;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("XShoot") != 0 || Input.GetAxisRaw("YShoot") != 0)
+        {
+            if (Time.time > timeToFire && !isShooting)
+            {
+                isShooting = true;
+                timeToFire += 1 / rateOfFire;
+                Fire();
+            }
+        }
+
+        /*else if (automaticFiring)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                if (Time.time > timeToFire)
+                {
+                    timeToFire += 1 / rateOfFire;
+                    Fire();
+                }
+            }
+        }*/
+        
+        
+    }
+
+    void Fire()
+    {
+        Debug.Log("Fired");
+
+        GameObject instance = Instantiate(projectile, transform.position, Quaternion.identity);
+        Vector2 direction = new Vector2(Input.GetAxisRaw("XShoot"), Input.GetAxisRaw("YShoot"));
+        instance.GetComponent<Rigidbody2D>().velocity = direction*projectileSpeed;
+
+        if (hasRecoil)
+        {
+            recoilObject.AddForce(-direction * recoilForce, ForceMode2D.Force);
+        }
+        isShooting = false;
+    }
+}
